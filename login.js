8fb806e066e13ae1b6f4467d94768581dbb784fe
Margo Kelley <margo.kelley13@gmail.com>
2022-04-21 14:23:19 -0400

    refactor card comp specifically to login

--- login.js
+++ login.js
@@ -8,7 +8,6 @@ function Login(){
 
   function handleCreate() {
     console.log(name, email, password);
-    if (!validate(name, "name")) return;
     if (!validate(email, "email")) return;
     if (!validate(password, "password")) return;
     ctx.users.push({ name, email, password, balance: 100 });
@@ -18,7 +17,7 @@ function Login(){
   function validate(field, label) {
     if (!field) {
       setStatus("Error: " + label);
-      setTimeout(() => setStatus(""), 3000);
+      setTimeout(() => setStatus(""), 5000);
       return false;
     }
     return true;
@@ -32,64 +31,57 @@ function Login(){
   }
 
   return (
-    <Card
-      bgcolor="grey"
-      txtcolor="black"
-      status={status}
-      header="LOGIN"
-      body={
-        show ? (
-          <>
-            Name
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="name"
-              placeholder="Enter name"
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-            />
-            <br />
-            Email address
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-            <br />
-            Password
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-            <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleCreate}
-            >
-              Create Account
-            </button>
-          </>
-        ) : (
-          <>
-            <h5>Success</h5>
-            <button type="submit" className="btn btn-light" onClick={clearForm}>
-              Add another account
-            </button>
-          </>
-        )
-      }
-    />
+    <div className="container">
+      <h1>Login</h1>
+      <Card
+        bgcolor="grey"
+        txtcolor="black"
+        status={status}
+        body={
+          show ? (
+            <>
+              Email address
+              <br />
+              <input
+                type="input"
+                className="form-control"
+                id="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              />
+              <br />
+              Password
+              <br />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.currentTarget.value)}
+              />
+              <br />
+              <button
+                type="submit"
+                className="btn btn-light"
+                onClick={handleCreate}
+              >
+                Login
+              </button>
+            </>
+          ) : (
+            <>
+              <h2 className="success-head">Login Successful.</h2>
+              <button type="submit" className="btn btn-light">
+                <a className="nav-link-bal" href="#/balance/">
+                  Go to Balance
+                </a>
+              </button>
+            </>
+          )
+        }
+      />
+    </div>
   );
 }
