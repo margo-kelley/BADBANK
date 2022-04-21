4ed8dec0eb1720effe504137564cd0535c2368bf
Margo Kelley <margo.kelley13@gmail.com>
2022-04-21 12:45:31 -0400

    add ReactRouterDOM

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function Spa() {
+  const Route = ReactRouterDOM.Route;
+  const Link = ReactRouterDOM.Link;
+  const HashRouter = ReactRouterDOM.HashRouter;
+
   return (
     <HashRouter>
       <NavBar/>
