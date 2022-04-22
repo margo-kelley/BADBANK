680bbd5d872c9f95227192d3c7949b66182bdbad
Margo Kelley <margo.kelley13@gmail.com>
2022-04-22 13:14:02 -0400

    card style- 100vw

--- context.js
+++ context.js
@@ -11,7 +11,7 @@ function Card(props) {
   }
 
   return (
-    <div className={classes()} style={{ maxWidth: "50em" }}>
+    <div className={classes()} style={{ maxWidth: "100vw" }}>
       <div className="card-body">
         {props.title && <h5 className="card-title">{props.title}</h5>}
         {props.text && <p className="card-text">{props.text}</p>}
