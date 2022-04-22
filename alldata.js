769ad48eb8c7ec2c808300f9c867dec464d3f760
Margo Kelley <margo.kelley13@gmail.com>
2022-04-22 13:13:06 -0400

    add bs cards

--- alldata.js
+++ alldata.js
@@ -2,10 +2,46 @@ function AllData(){
   const ctx = React.useContext(UserContext);
   return (
     <>
-      <h1>
-        All Data!<br/>
-        {JSON.stringify(ctx)}
+      <h1 className="allData-head">
+        All User Data:
+        <br />
       </h1>
+      <div className="container-data">
+        <div className="row align-items-center">
+        <div className="col">
+          <div className="card">
+        <div className="card-header">Name</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{ctx.name}</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+        </div>
+        <div className="col">
+          <div className="card">
+        <div className="card-header">Email</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{ctx.email}</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+        </div>
+        <div className="col">
+        <div className="card">
+        <div className="card-header">Password</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{ctx.password}</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+        </ul>
+      </div>
+        </div>
+      </div>
+      </div>
+      
+      <p>{JSON.stringify(ctx)}</p>
     </>
   );
 }
