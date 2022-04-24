7b54a5c6b0144675dd1ec3d54fbb8aace4f8bc82
Margo Kelley <margo.kelley13@gmail.com>
2022-04-23 23:32:28 -0400

    fix vh for Card comp

--- context.js
+++ context.js
@@ -13,7 +13,6 @@ function Card(props) {
   return (
     <div className={classes()} style={{ maxWidth: "100vw" }}>
       <div className="card-body">
-        {props.title && <h5 className="card-title">{props.title}</h5>}
         {props.text && <p className="card-text">{props.text}</p>}
         {props.body}
         {props.status && <div id="createStatus">{props.status}</div>}
