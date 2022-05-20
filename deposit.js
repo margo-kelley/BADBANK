82d0e7e955c0355aadae36ab8a48585d1291c539
Margo Kelley <margo.kelley13@gmail.com>
2022-05-20 15:41:08 -0400

    card updated for depo page use

--- deposit.js
+++ deposit.js
@@ -1,95 +1,41 @@
 function Deposit(){
-  const [show, setShow] = React.useState(true);
+  // const [show, setShow] = React.useState(true);
   const [status, setStatus] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [balance, setBalance] = React.useState("");
   const ctx = React.useContext(UserContext);
 
-  function handleCreate() {
-    console.log(name, email, password);
-    if (!validate(name, "name")) return;
-    if (!validate(email, "email")) return;
-    if (!validate(password, "password")) return;
-    ctx.users.push({ name, email, password, balance: 100 });
-    setShow(false);
-  }
-
-  function validate(field, label) {
-    if (!field) {
-      setStatus("Error: " + label);
-      setTimeout(() => setStatus(""), 3000);
-      return false;
-    }
-    return true;
-  }
-
-  function clearForm() {
-    setName("");
-    setEmail("");
-    setPassword("");
-    setShow(true);
+  function depositCash() {
+    let amount = document.getElementById('deposit').value;
+    console.log(amount);
+    let balance = 100;
+    if (amount > 0) {
+      let deposit = amount + balance;
+      console.log(deposit);
+      return deposit;
+    } return;
   }
 
   return (
-    <Card
+    <div className="container-fluid">
+      <div className="header">Deposit</div>
+      <Card
       bgcolor="grey"
       txtcolor="black"
       status={status}
-      header="DEPOSIT"
       body={
-        show ? (
-          <>
-            Name
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="name"
-              placeholder="Enter name"
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-            />
-            <br />
-            Email address
-            <br />
-            <input
-              type="input"
-              className="form-control"
-              id="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-            <br />
-            Password
-            <br />
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-            <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleCreate}
-            >
-              Create Account
-            </button>
-          </>
-        ) : (
-          <>
-            <h5>Success</h5>
-            <button type="submit" className="btn btn-light" onClick={clearForm}>
-              Add another account
-            </button>
-          </>
-        )
+        <>
+          <label htmlFor="balance">Balance</label>
+          <p>where the # should be</p>
+          <br />
+          <label htmlFor="deposit amount">Deposit Amount</label>
+          <br />
+          <input type="number" className="form-control" id="deposit" min="0" />
+          <br />
+          <button className=" btn btn-light" onClick={depositCash}>Add $$$</button>
+        </>
       }
     />
+    </div>
+    
   );
 }
