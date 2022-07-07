868700c9aeeb390bd6f55100ade92297e16ae08e
Margo Kelley <margo.kelley13@gmail.com>
2022-07-07 18:24:39 -0400

    final INDEX

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function Spa() {
   const Route = ReactRouterDOM.Route;
-  const Link = ReactRouterDOM.Link;
+  // const Link = ReactRouterDOM.Link;
   const HashRouter = ReactRouterDOM.HashRouter;
 
   return (
     <HashRouter>
       <NavBar/>
-      <UserContext.Provider value={{users:[{name:'peter',email:'peter@mit.edu',password:'secret',balance:100}]}}>
+      <UserContext.Provider value={{users:[{id: 1, name:'peter', email:'peter@mit.edu', password:'secret', balance:100}]}}>
         <Route path="/" exact component={Home} />
         <Route path="/CreateAccount/" component={CreateAccount} />
         <Route path="/deposit/" component={Deposit} />
